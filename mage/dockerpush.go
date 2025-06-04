dca1a5d6830c03fb7766eaf2772896c1f905022b
Zack Williams <zack.williams@intel.com>
2025-06-04 13:52:35 -0700

    Fix container build process to use make targets

--- mage/dockerpush.go
+++ mage/dockerpush.go
@@ -58,21 +58,17 @@ func pushImage(imageName string, chartName string) error {
 }
 
 func pushNexusCompilerImage() error {
-	appVersion := getNexusCompilerTag()
+	appVersion := getNexusVersion()
 	imageName := "nexus/compiler/amd64"
 	registry := OpenEdgePlatformContainerRegistry
-	if err := tryToCreateECRRepository(fmt.Sprintf("edge-orch/common/%s", imageName)); err != nil {
-		return err
-	}
+
 	return inspectAndPushImage(registry, imageName, appVersion)
 }
 
 func pushOpenAPIGeneratorImage() error {
-	appVersion := getNexusCompilerTag()
+	appVersion := getNexusVersion()
 	imageName := "nexus/openapi-generator"
 	registry := OpenEdgePlatformContainerRegistry
-	if err := tryToCreateECRRepository(fmt.Sprintf("edge-orch/common/%s", imageName)); err != nil {
-		return err
-	}
+
 	return inspectAndPushImage(registry, imageName, appVersion)
 }
